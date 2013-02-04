98378e1e978c68ceea25386834544c0f43a63eea
colinmegill <colinmegill@gmail.com>
2013-02-03 17:01:17 -0800

    frontpage up and running

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#backbone_intensive, #node_intensive, #advanced_javascript').hover(function(){
+		$(this).toggleClass("highlight")
+	});
+	
+	$('#backbone_intensive').click(function(){
+		window.location
+	});
+});
