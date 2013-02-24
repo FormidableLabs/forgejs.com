4e063da0f35c1da6536c4ade057b8b21c4b81806
Ryan Eastridge <ryan@eastridge.me>
2013-02-24 13:17:43 -0800

    Remove app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('#backbone_intensive, #node_intensive, #advanced_javascript').hover(function(){
-		$(this).toggleClass("highlight")
-	});
-	
-	$('#backbone_intensive').click(function(){
-		window.location
-	});
-});
